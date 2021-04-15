b4658a955039880c635c214a207a6d2fcf99a2db
Maxpira <pira.max@gmail.com>
2021-04-15 10:32:46 -0300

    iniciando curso java basico

---
+++ ImprimirVariavel.java
@@ -0,0 +1,12 @@
+public class ImprimirVariavel {
+        public static void main(String[] args) {
+          
+            String nome = "MAX ALEXANDRE";
+            int idade = 46;
+            float salario = 5000f;
+        System.out.println("meu nome \n" +nome+ " idade: "  +idade);
+       // System.out.println(idade>=46 || salario >= 2000);
+        
+    }
+    
+}
